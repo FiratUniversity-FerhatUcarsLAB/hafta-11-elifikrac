40ee024d726d22d09ccbc6d9a69f53ee071796c8
elifikrac <250541010@firat.edu.tr>
2025-12-03 21:42:17 +0300

    Update Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -3,7 +3,7 @@ public class Exercise4_5 {
     public static void zoop(String fred, int bob) {
         System.out.println(fred);
         if (bob == 5) {
-            ping("not ");
+            ping(" not ");
         } else {
             System.out.println("!");
         }
@@ -12,16 +12,26 @@ public class Exercise4_5 {
     public static void main(String[] args) {
         int bizz = 5;
         int buzz = 2;
-        zoop("just for", bizz);
+        zoop(" just for ", bizz);
         clink(2 * buzz);
     }
 
     public static void clink(int fork) {
-        System.out.print("It's ");
-        zoop("breakfast ", fork);
+        System.out.print("It ’s ");
+        zoop(" breakfast ", fork);
     }
 
     public static void ping(String strangStrung) {
-        System.out.println("any " + strangStrung + "more ");
+        System.out.println(" any " + strangStrung + " more ");
     }
 }
+
+/*
+Program calistiginda yaklasik olarak su cikti olusur (bosluklar dosyadaki stringlere gore):
+
+ just for 
+ any  not  more 
+It ’s  breakfast 
+!
+
+*/
